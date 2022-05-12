251d354677a418960a0b042a6cfcb820d1356135
Atlas Sproson <atlas.sproson@r2-factory.com>
2022-05-12 13:10:48 +0100

    Adds base Header code

---
+++ tutor-project/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar color="black">
+    <v-toolbar-title id="header-title">Header Component</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<style>
+#header-title {
+  /* Change font color here */
+}
+</style>
