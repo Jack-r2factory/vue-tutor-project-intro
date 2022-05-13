71b0e9a293518a05dd76dfdbae2077a74c59df91
Atlas Sproson <atlas.sproson@r2-factory.com>
2022-05-13 11:59:05 +0100

    Creates About

---
+++ tutor-project/src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="about-container">
+    <p>
+      {{ message }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello there!",
+    };
+  },
+};
+</script>
+
+<style>
+#about-container {
+  text-align: center;
+}
+</style>
